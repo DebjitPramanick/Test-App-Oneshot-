aa0df529fd10792361adea2f003518690808f53d
DebjitPramanick <dpramanick9990@gmail.com>
2023-03-09 21:23:02 +0530

    Fix: Server deployment #1

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 export const ENV = process.env.NODE_ENV
 
-export const API_URL = ENV === 'deveolpment' ? 'http://localhost:8080/api' : ''
+export const API_URL = ENV === 'deveolpment' ? 'http://localhost:8080/api' : process.env.REACT_APP_API_URL
 
 export const TOKEN  = localStorage.getItem("app_token") || ""
